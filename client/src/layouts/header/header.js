fbce83c4a78ab0fa50fb1507cb7af8a28ce3b129
Hung Chan <luuchanhunggs1@gmail.com>
2023-10-16 14:21:30 +0700

    navbar always on top

--- client/src/layouts/header/header.js
+++ client/src/layouts/header/header.js
@@ -17,10 +17,10 @@ const navigation = [
 
 export default function Example() {
   return (
-    <Disclosure as="nav" className="bg-white">
+    <Disclosure as="nav" className=" bg-white sticky top-0 ">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 ">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -2,6 +2,58 @@
   
 export default function Home() {
   return (
+    <>
+        <h1 className='center'>Home</h1>
     <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    <h1 className='center'>Home</h1>
+    </>
+
+
+    
   );
 }
