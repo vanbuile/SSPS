cad6abe994cff73b1d18e779d1b7ff308f11b41c
Phuc <93708621+bi151103@users.noreply.github.com>
2023-10-20 19:46:11 +0700

    Update paperAdmin.js - gộp component (chỉnh kiểu file và cập nhật số trang in vào đây)

--- client/src/pages/admin/paperAdmin.js
+++ client/src/pages/admin/paperAdmin.js
@@ -1,11 +1,30 @@
 import React from 'react'
-
+import AddingTypeForm from '../../components/AddingTypeForm';
+import RemovingTypeForm from '../../components/RemovingTypeForm';
+import AdjustingDefaultPageNumForm from '../../components/AdjustingDefaultPageNumForm';
 export default function PaperAdmin() {
+	const scrollToComponent = (componentId) => {
+        const component = document.getElementById(componentId);
+        if (component) {
+            component.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
 	return (
-
-		<div >
-			
- 12131242131212321
+		<div>
+			<div className="flex text-[20px] justify-between">
+                		<button className="w-40 h-30 bg-[#0F6CBF] text-[#F5F5F5] p-2 rounded-md" onClick={() => scrollToComponent("AddingTypeForm")}>Thêm kiểu file</button>
+		                <button className="w-40 h-30 bg-[#0F6CBF] text-[#F5F5F5] p-2 rounded-md" onClick={() => scrollToComponent("RemovingTypeForm")}>Xóa kiểu file</button>
+		                <button className="w-40 h-30 bg-[#0F6CBF] text-[#F5F5F5] p-2 rounded-md" onClick={() => scrollToComponent("AdjustingDefaultPageNumForm")}>Điều chỉnh số lượng trang in</button>
+	            	</div>
+			<div id="AddingTypeForm">
+				<AddingTypeForm />
+			</div>
+			<div id="RemovingTypeForm">
+				<RemovingTypeForm />
+			</div>
+			<div id="AdjustingDefaultPageNumForm">
+				<AdjustingDefaultPageNumForm />
+		    	</div>
 		</div>
 	)
 }
