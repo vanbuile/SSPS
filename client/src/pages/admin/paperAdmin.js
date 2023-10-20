ebbd93c9fcc03a1960500d4447c66415bd45c7c4
Phuc <93708621+bi151103@users.noreply.github.com>
2023-10-20 21:12:32 +0700

    Update paperAdmin.js - chỉ hiển thị một component khi ấn chọn nút tương ứng với component đó (đọc thêm mô tả)
    Trang paperAdmin có 3 components. Ban đầu ấn vào sẽ chỉ hiện 3 cái nút dẫn đến mỗi component. SPSO ấn vào đâu thì display component đó (mấy cái kia bị hide).

--- client/src/pages/admin/paperAdmin.js
+++ client/src/pages/admin/paperAdmin.js
@@ -1,30 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
 import AddingTypeForm from '../../components/AddingTypeForm';
 import RemovingTypeForm from '../../components/RemovingTypeForm';
 import AdjustingDefaultPageNumForm from '../../components/AdjustingDefaultPageNumForm';
+
 export default function PaperAdmin() {
-	const scrollToComponent = (componentId) => {
+    const [activeComponent, setActiveComponent] = useState(null);
+
+    const scrollToComponent = (componentId) => {
+        setActiveComponent(componentId);
         const component = document.getElementById(componentId);
         if (component) {
             component.scrollIntoView({ behavior: 'smooth' });
         }
     };
-	return (
-		<div>
-			<div className="flex text-[20px] justify-between">
-                		<button className="w-40 h-30 bg-[#0F6CBF] text-[#F5F5F5] p-2 rounded-md" onClick={() => scrollToComponent("AddingTypeForm")}>Thêm kiểu file</button>
-		                <button className="w-40 h-30 bg-[#0F6CBF] text-[#F5F5F5] p-2 rounded-md" onClick={() => scrollToComponent("RemovingTypeForm")}>Xóa kiểu file</button>
-		                <button className="w-40 h-30 bg-[#0F6CBF] text-[#F5F5F5] p-2 rounded-md" onClick={() => scrollToComponent("AdjustingDefaultPageNumForm")}>Điều chỉnh số lượng trang in</button>
-	            	</div>
-			<div id="AddingTypeForm">
-				<AddingTypeForm />
-			</div>
-			<div id="RemovingTypeForm">
-				<RemovingTypeForm />
-			</div>
-			<div id="AdjustingDefaultPageNumForm">
-				<AdjustingDefaultPageNumForm />
-		    	</div>
-		</div>
-	)
+
+    return (
+        <div>
+            <div className="flex text-[20px] justify-between">
+                <button
+                    className={`w-40 h-30 bg-[#0F6CBF] text-[#F5F5F5] p-2 rounded-md ${activeComponent === 'AddingTypeForm' ? 'bg-gray-500' : ''}`}
+                    onClick={() => scrollToComponent("AddingTypeForm")}
+                >
+                    Thêm kiểu file
+                </button>
+                <button
+                    className={`w-40 h-30 bg-[#0F6CBF] text-[#F5F5F5] p-2 rounded-md ${activeComponent === 'RemovingTypeForm' ? 'bg-gray-500' : ''}`}
+                    onClick={() => scrollToComponent("RemovingTypeForm")}
+                >
+                    Xóa kiểu file
+                </button>
+                <button
+                    className={`w-40 h-30 bg-[#0F6CBF] text-[#F5F5F5] p-2 rounded-md ${activeComponent === 'AdjustingDefaultPageNumForm' ? 'bg-gray-500' : ''}`}
+                    onClick={() => scrollToComponent("AdjustingDefaultPageNumForm")}
+                >
+                    Điều chỉnh số lượng trang in
+                </button>
+            </div>
+            <div id="AddingTypeForm" style={{ display: activeComponent === 'AddingTypeForm' ? 'block' : 'none' }}>
+                <AddingTypeForm />
+            </div>
+            <div id="RemovingTypeForm" style={{ display: activeComponent === 'RemovingTypeForm' ? 'block' : 'none' }}>
+                <RemovingTypeForm />
+            </div>
+            <div id="AdjustingDefaultPageNumForm" style={{ display: activeComponent === 'AdjustingDefaultPageNumForm' ? 'block' : 'none' }}>
+                <AdjustingDefaultPageNumForm />
+            </div>
+        </div>
+    );
 }
