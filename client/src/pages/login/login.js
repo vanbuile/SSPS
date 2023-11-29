23e067c27960a8eb012dec00144183f8b52caa8b
Phuc Dang <dangphanminhphuctbag@gmail.com>
2023-11-30 02:07:29 +0700

    a

--- client/src/pages/login/login.js
+++
@@ -1,7 +0,0 @@
-
-export default function Login() {
-    return (
-      <h1 className='center'>Login</h1>
-    );
-  }
-  
--- server/routes/pageNumber.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const {UpdatePageNumber} = require("../BusinessLayer/PageNumber/PageNumber");
-
-router.get('/UpdatePageNumber', UpdatePageNumber);
-module.exports = router
