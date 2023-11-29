9090c88ee18154faba4817f3fe84d5108418e614
Hung Chan <luuchanhunggs1@gmail.com>
2023-11-29 23:20:52 +0700

    reafactor

--- server/routes/buy.js
+++ server/routes/buy.js
@@ -1,147 +1,20 @@
-const e = require("express");
+
 const express = require("express");
 const router = express.Router();
-const moment = require("moment");
+const {CreatePaymentUrl, VnpayReturn} = require('../BusinessLayer/Buy/Buy');
+
 
 router.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-router.post("/create_payment_url", (req, res) => {
-  process.env.TZ = "Asia/Ho_Chi_Minh";
+router.post("/create_payment_url", CreatePaymentUrl);
 
-  let date = new Date();
-  let createDate = moment(date).format("YYYYMMDDHHmmss");
+router.get('/vnpay_return', VnpayReturn);
 
-  let ipAddr =
-    req.headers["x-forwarded-for"] ||
-    req.connection.remoteAddress ||
-    req.socket.remoteAddress ||
-    req.connection.socket.remoteAddress;
 
-  let tmnCode = "PB6LSZKB";
-  let secretKey = "JOUNKJFVQTKYYZAANRSSUVXKNXOKQEPV";
-  let vnpUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-  let returnUrl = "http://localhost:3000";
-  let orderId = moment(date).format("DDHHmmss");
-  let amount = 100000;
-  let bankCode = "";
 
-  let locale = "vn";
 
-  let currCode = "VND";
-  let vnp_Params = {};
-  vnp_Params["vnp_Version"] = "2.1.0";
-  vnp_Params["vnp_Command"] = "pay";
-  vnp_Params["vnp_TmnCode"] = tmnCode;
-  vnp_Params["vnp_Locale"] = locale;
-  vnp_Params["vnp_CurrCode"] = currCode;
-  vnp_Params["vnp_TxnRef"] = orderId;
-  vnp_Params["vnp_OrderInfo"] = "Thanh toan cho ma GD:" + orderId;
-  vnp_Params["vnp_OrderType"] = "other";
-  vnp_Params["vnp_Amount"] = amount * 100;
-  vnp_Params["vnp_ReturnUrl"] = returnUrl;
-  vnp_Params["vnp_IpAddr"] = ipAddr;
-  vnp_Params["vnp_CreateDate"] = createDate;
-  if (bankCode !== null && bankCode !== "") {
-    vnp_Params["vnp_BankCode"] = bankCode;
-  }
 
-  vnp_Params = sortObject(vnp_Params);
-
-  let querystring = require("qs");
-  let signData = querystring.stringify(vnp_Params, { encode: false });
-  let crypto = require("crypto");
-  let hmac = crypto.createHmac("sha512", secretKey);
-  let signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
-  vnp_Params["vnp_SecureHash"] = signed;
-  vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: false });
-  //send vnpUrl to client
-  res.send(vnpUrl);
-});
-
-function sortObject(obj) {
-  let sorted = {};
-  let str = [];
-  let key;
-  for (key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      str.push(encodeURIComponent(key));
-    }
-  }
-  str.sort();
-  for (key = 0; key < str.length; key++) {
-    sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
-  }
-  return sorted;
-}
-
-router.get("/vnpay_ipn", function (req, res, next) {
-  console.log("IPN");
-  let vnp_Params = req.query;
-  let secureHash = vnp_Params["vnp_SecureHash"];
-
-  let orderId = vnp_Params["vnp_TxnRef"];
-  let rspCode = vnp_Params["vnp_ResponseCode"];
-
-  delete vnp_Params["vnp_SecureHash"];
-  delete vnp_Params["vnp_SecureHashType"];
-
-  vnp_Params = sortObject(vnp_Params);
-  let config = require("config");
-  let secretKey = config.get("vnp_HashSecret");
-  let querystring = require("qs");
-  let signData = querystring.stringify(vnp_Params, { encode: false });
-  let crypto = require("crypto");
-  let hmac = crypto.createHmac("sha512", secretKey);
-  let signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
-
-  let paymentStatus = "0"; // Giả sử '0' là trạng thái khởi tạo giao dịch, chưa có IPN. Trạng thái này được lưu khi yêu cầu thanh toán chuyển hướng sang Cổng thanh toán VNPAY tại đầu khởi tạo đơn hàng.
-  //let paymentStatus = '1'; // Giả sử '1' là trạng thái thành công bạn cập nhật sau IPN được gọi và trả kết quả về nó
-  //let paymentStatus = '2'; // Giả sử '2' là trạng thái thất bại bạn cập nhật sau IPN được gọi và trả kết quả về nó
-
-  let checkOrderId = true; // Mã đơn hàng "giá trị của vnp_TxnRef" VNPAY phản hồi tồn tại trong CSDL của bạn
-  let checkAmount = true; // Kiểm tra số tiền "giá trị của vnp_Amout/100" trùng khớp với số tiền của đơn hàng trong CSDL của bạn
-  if (secureHash === signed) {
-    //kiểm tra checksum
-    if (checkOrderId) {
-      if (checkAmount) {
-        if (paymentStatus == "0") {
-          //kiểm tra tình trạng giao dịch trước khi cập nhật tình trạng thanh toán
-          if (rspCode == "00") {
-            //thanh cong
-            //paymentStatus = '1'
-            // Ở đây cập nhật trạng thái giao dịch thanh toán thành công vào CSDL của bạn
-            res.status(200).json({ RspCode: "00", Message: "Success" });
-            console.log("IPN Success");
-          } else {
-            //that bai
-            //paymentStatus = '2'
-            // Ở đây cập nhật trạng thái giao dịch thanh toán thất bại vào CSDL của bạn
-            console.log("IPN Fail");
-            res.status(200).json({ RspCode: "00", Message: "Success" });
-          }
-        } else {
-          console.log("Order already updated");
-          res
-            .status(200)
-            .json({
-              RspCode: "02",
-              Message: "This order has been updated to the payment status",
-            });
-        }
-      } else {
-        console.log("Amount not valid");
-        res.status(200).json({ RspCode: "04", Message: "Amount invalid" });
-      }
-    } else {
-      console.log("Order not found");
-      res.status(200).json({ RspCode: "01", Message: "Order not found" });
-    }
-  } else {
-    console.log("Invalid checksum");
-    res.status(200).json({ RspCode: "97", Message: "Checksum failed" });
-  }
-});
 module.exports = router;
-1;
+
