b4f0399bb04cad5c212530e34bee07be2ab004d2
anhngocdohaha <anhngocdohaha@gmail.com>
2023-11-30 22:55:06 +0700

    merge với commit Edit fe share file - be Share file (search,filter, comment,rating, view, delete)

--- client/src/pages/shared/shared.js
+++ client/src/pages/shared/shared.js
@@ -1,68 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import { Link } from 'react-router-dom';
 import { FaSearch } from 'react-icons/fa';
 import { FaFilter } from 'react-icons/fa';
+import axios from "axios";
+import APIs from "../../util/API.js";
 export default function Shared() {
   const [isInputFocused, setInputFocused] = useState(false);
   const [isFilterOpen, setIsFilterOpen] = useState(false);
   const [selectedFilter, setSelectedFilter] = useState(""); 
-  const articles = [
-    { 
-      title: "Nguyên lý về mối liên hệ phổ biến",
-      description: "Phép biện chứng duy vật được xây dựng trên cơ sở một hệ thống những nguyên lý,những phạm trù cơ bản, những quy luật phổ biến phản ánh đúng đắn hiện thực.........biến phản ánh " },
-    {
-      title: "Ôn tập cuối kì",
-      description: "Môn này năm học hk222",
-    },
-    {
-      title: "Hóa học vô cơ (Chem2022)",
-      description: "Một lượng nhỏ khí hydrogen có thể hoà tan trong hầu hết kim loại nóng chảy vì hydrogen có kích thước bé nên có thể xâm nhập vào các thể tích trống của kim loại",
-    },
-    {
-      title: "Nguyên lý về mối liên hệ phổ biến",
-      description: "Phép biện chứng duy vật được xây dựng trên cơ sở một hệ thống những nguyên lý,những phạm trù cơ bản, những quy luật phổ biến phản ánh đúng đắn hiện thực." },
-    {
-      title: "Ôn tập cuối kì",
-      description: "Môn này năm học hk222",
-    },
-    {
-      title: "Hóa học vô cơ (Chem2022)",
-      description: "Một lượng nhỏ khí hydrogen có thể hoà tan trong hầu hết kim loại nóng chảy vì hydrogen có kích thước bé nên có thể xâm nhập vào các thể tích trống của kim loại",
-    },
-    {
-      title: "Nguyên lý về mối liên hệ phổ biến",
-      description: "Phép biện chứng duy vật được xây dựng trên cơ sở một hệ thống những nguyên lý,những phạm trù cơ bản, những quy luật phổ biến phản ánh đúng đắn hiện thực." },
-    {
-      title: "Ôn tập cuối kì",
-      description: "Môn này năm học hk222",
-    },
-    {
-      title: "Hóa học vô cơ (Chem2022)",
-      description: "Một lượng nhỏ khí hydrogen có thể hoà tan trong hầu hết kim loại nóng chảy vì hydrogen có kích thước bé nên có thể xâm nhập vào các thể tích trống của kim loại",
-    },
-    {
-      title: "Nguyên lý về mối liên hệ phổ biến",
-      description: "Phép biện chứng duy vật được xây dựng trên cơ sở một hệ thống những nguyên lý,những phạm trù cơ bản, những quy luật phổ biến phản ánh đúng đắn hiện thực." },
-    {
-      title: "Ôn tập cuối kì",
-      description: "Môn này năm học hk222",
-    },
-    {
-      title: "Hóa học vô cơ (Chem2022)",
-      description: "Một lượng nhỏ khí hydrogen có thể hoà tan trong hầu hết kim loại nóng chảy vì hydrogen có kích thước bé nên có thể xâm nhập vào các thể tích trống của kim loại",
-    },
-    {
-      title: "Nguyên lý về mối liên hệ phổ biến",
-      description: "Phép biện chứng duy vật được xây dựng trên cơ sở một hệ thống những nguyên lý,những phạm trù cơ bản, những quy luật phổ biến phản ánh đúng đắn hiện thực." },
-    {
-      title: "Ôn tập cuối kì",
-      description: "Môn này năm học hk222",
-    },
-    {
-      title: "Hóa học vô cơ (Chem2022)",
-      description: "Một lượng nhỏ khí hydrogen có thể hoà tan trong hầu hết kim loại nóng chảy vì hydrogen có kích thước bé nên có thể xâm nhập vào các thể tích trống của kim loại",
-    },
-  ];
+  const [dataFromServer, setDataFromServer] = useState([]);
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [searchResults, setSearchResults] = useState([]); 
+  const [filterKeyword, setFilterKeyword] = useState([]); 
+
+  useEffect(async () => {
+    const fetchData = async () => {
+      try {
+        const url = APIs.APIshareFile + "/";
+        const response = await axios.get(url);
+        setDataFromServer(response.data.data); 
+        //console.log(response.data);
+      } catch (error) {
+        if (error.response) {
+          console.log(error.response.data.message);
+        }
+      }
+    };
+
+    fetchData();
+  }, []);
+  const handleSearch = async () => {
+    try {
+      let url = APIs.APIshareFile + "/";
+      if (filterKeyword) {
+        url = `${APIs.APIshareFile}/?search=${searchKeyword}&sort=${filterKeyword}`;
+      } else {
+        url = `${APIs.APIshareFile}/?search=${searchKeyword}`;
+      }
+      const response = await axios.get(url);
+      setSearchResults(response.data.data);
+      //console.log(response.data);
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data.message);
+      }
+    }
+  };
+  const handleFilterSelect = async (value) => {
+    setSelectedFilter(value); 
+    setFilterKeyword(value);
+    setIsFilterOpen(false);
+    try {
+      let url = APIs.APIshareFile + "/";
+      if (searchKeyword) {
+        url = `${APIs.APIshareFile}/?search=${searchKeyword}&sort=${value}`;
+      } else {
+        url = `${APIs.APIshareFile}/?sort=${value}`;
+      }
+      const response = await axios.get(url);
+      setSearchResults(response.data.data);
+      //console.log(response.data);
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data.message);
+      }
+    }
+  };
   const styles = {
     topframe: {
       width: "auto",
@@ -96,7 +99,7 @@ export default function Shared() {
       cursor: "pointer",
       border:"1px solid #0F6CBF",
       borderRadius:"15px",
-      padding:"0 30px 0 0",
+      padding:"0 30px 0 30px",
       height:"40px",
       display: "flex",
       flexDirection: "row",
@@ -147,9 +150,11 @@ export default function Shared() {
   const handleInputBlur = () => {
     setInputFocused(false);
   };
-  const handleFilterSelect = (value) => {
-    setSelectedFilter(value); 
-    setIsFilterOpen(false);
+  
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch(); 
+    }
   };
   const handleAuthorization = (role) => {
     const cookies = document.cookie.split('; ');
@@ -172,38 +177,48 @@ export default function Shared() {
               placeholder="Tìm kiếm tài liệu bạn cần ..."
               onFocus={handleInputFocus}
               onBlur={handleInputBlur}
+              value={searchKeyword}
+              onChange={(e) => setSearchKeyword(e.target.value)}
+              onKeyDown={handleKeyPress} 
             />
+            
           </div>
-          <div style={styles.filterButton} onClick={() => setIsFilterOpen(!isFilterOpen)}>
-            <FaFilter style={styles.filterIcon} /> Filter
+          <div style={styles.filterButton}>
+          <FaFilter isOpen={isFilterOpen} onClick={() => setIsFilterOpen(!isFilterOpen)} /> Filter
           </div>
         </div>
         <div style={styles.filterDialog}>
-          <label>Sắp xếp theo:</label>
           <select
             style={styles.select}
             value={selectedFilter}
             onChange={(e) => handleFilterSelect(e.target.value)}
           >
-            <option value="timeAscending">Thời gian tăng dần</option>
-            <option value="timeDescending">Thời gian giảm dần</option>
-            <option value="rating">Đánh giá</option>
-            <option value="sizeAscending">Kích thước tăng dần</option>
-            <option value="sizeDescending">Kích thước giảm dần</option>
+            <option value="star_desc">Đánh giá cao</option>
+            <option value="star_asc">Đánh giá thấp</option>
           </select>
         </div>
         <div style={styles.content}>
-        {articles.map((article, index) => (
-          <div key={index} style={styles.article}>
-            <h2 style={{ color: "#0F6CBF", fontSize: "120%", paddingBottom:"5px"}}>
-              <Link to={'/detail'}>{article.title}</Link>
-            </h2>
-            <p style={{color:"gray"}}>{article.description}</p>
-          </div>
-        ))}
+        {searchResults.length > 0 ? (
+    searchResults.map((article, index) => (
+      <div key={index} style={styles.article}>
+        <h2 style={{ color: "#0F6CBF", fontSize: "120%", paddingBottom: "5px" }}>
+          <Link to={`/detail/${article.id}`}>{article.name}</Link>
+        </h2>
+        <p style={{ color: "gray" }}>{article.description}</p>
+      </div>
+    ))
+  ) : (
+    dataFromServer.map((article, index) => (
+      <div key={index} style={styles.article}>
+        <h2 style={{ color: "#0F6CBF", fontSize: "120%", paddingBottom: "5px" }}>
+          <Link to={`detail/${article.ID}`}>{article.name}</Link>
+        </h2>
+        <p style={{ color: "gray" }}>{article.description}</p>
+      </div>
+    ))
+  )}
       </div>
       </div>
     );
   }
 }
-
--- server/SSPS.sql
+++ server/SSPS.sql
@@ -46,7 +46,8 @@ CREATE TABLE IF NOT EXISTS FILE (
     name VARCHAR(255),
     description VARCHAR(255),
     link VARCHAR(255),
-    isShare INT -- nếu =0 thì không share -> link = nullptr, nếu =1 thì share -> link != nullptr
+    isShare INT, 
+    score INT DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS PRINTING (
@@ -74,7 +75,7 @@ CREATE TABLE IF NOT EXISTS RATING (
 CREATE TABLE IF NOT EXISTS COMMENT (
     MSSV VARCHAR(7),
     id_file INT,
-    Content nvarchar(255),
+    Content TEXT,
     date DATETIME,
     PRIMARY KEY (MSSV, id_file),
     FOREIGN KEY (MSSV) REFERENCES STUDENT(MSSV),
@@ -102,3 +103,20 @@ CREATE TABLE IF NOT EXISTS FILEHAVETYPE(
     FOREIGN KEY (id_file) REFERENCES FILE(ID),
     FOREIGN KEY (id_type) REFERENCES FILETYPE(ID)
 );
+---trigger update score of file
+DELIMITER //
+
+CREATE TRIGGER update_score_after_rating_insert
+AFTER INSERT ON RATING
+FOR EACH ROW
+BEGIN
+    DECLARE avg_score DECIMAL(5, 2);
+    
+    -- Calculate the average score for the file
+    SELECT AVG(star) INTO avg_score FROM RATING WHERE id_file = NEW.id_file;
+    
+    -- Update the score in the FILE table
+    UPDATE FILE SET score = avg_score WHERE ID = NEW.id_file;
+END//
+
+DELIMITER ;
